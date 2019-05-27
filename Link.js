eeeaabd6715b2159f1d703cdbc46cb11a03c42bf
fst <1172088356@qq.com>
2019-05-27 09:42:53 +0800

    link

---
+++ Link.js
@@ -0,0 +1,16 @@
+class Link {
+    constructor() {
+        this.node = {
+            index: null,
+            next: null,
+            size: 0
+        };
+    }
+    insert(index, data) {
+        while(this.node.index && this.node.index >= 0) {
+            
+        }
+    }
+}
+
+export default Link;
